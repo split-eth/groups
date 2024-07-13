26b1902c8fc062ee5a247f19026360c7780562ec
Chaimaa Nairi <83820363+ChaimaaNairi@users.noreply.github.com>
2024-07-13 07:52:34 +0200

    GroupFactory

--- contracts/GroupFactory.sol
+++ contracts/GroupFactory.sol
@@ -1,5 +1,5 @@
 FactoryGroup.sol
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
