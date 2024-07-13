cb1ddd127969e17817f9494aa24a4929c196e465
Chaimaa Nairi <83820363+ChaimaaNairi@users.noreply.github.com>
2024-07-13 17:11:17 +0200

    test

--- test/GroupFactory.test.ts
+++ test/GroupFactory.test.ts
@@ -1,6 +1,5 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { ContractFactory, Contract, Signer } from 'ethers';
 
 describe("GroupFactory", function () {
   let GroupFactory: any;
@@ -21,39 +20,38 @@ describe("GroupFactory", function () {
     const tx =  groupFactory.deploymentTransaction();
     await tx.wait();
   });
-
+  
   describe("Functionality", function () {
     it("Should create a new Group contract", async function () {
-      const groupName = "Test Group";
-      const groupHash = ethers.keccak256(ethers.toUtf8Bytes(groupName));
+      const ownerAddress = await addr1.getAddress();
+      const tokenAddress = ethers.AddressZero;
+      const groupHash = ethers.keccak256(ethers.toUtf8Bytes("test-hash"));
 
-      await groupFactory.createGroup(groupName, addr1.address);
+      const tx = await groupFactory.createGroup(ownerAddress, tokenAddress, groupHash);
+      await tx.wait();
 
       const groupAddress = await groupFactory.groups(groupHash);
+      expect(groupAddress).to.not.equal(ethers.AddressZero);
 
       const group = await ethers.getContractAt("Group", groupAddress);
-      expect(group).to.exist;
-
       const admin = await group.admin();
-      expect(admin).to.equal(addr1.address);
+      expect(admin).to.equal(ownerAddress);
     });
-
-    it("Should revert if trying to create a group with an existing hash", async function () {
-      const groupName = "Test Group";
-      const groupHash = ethers.keccak256(ethers.toUtf8Bytes(groupName));
-
-      await groupFactory.createGroup(groupName, addr1.address);
-
-      await expect(groupFactory.createGroup(groupName, addr2.address)).to.be.revertedWith("Group already exists");
-    });
-
-    
-  });
-
-  describe("Event", function () {
-
-  });
-
+  
+      it("Should revert if trying to create a group with an existing hash", async function () {
+        const ownerAddress = await addr1.getAddress();
+        const tokenAddress = ethers.AddressZero;
+        const groupHash = ethers.keccak256(ethers.toUtf8Bytes("test-hash"));
+  
+        await groupFactory.createGroup(ownerAddress, tokenAddress, groupHash);
+        await expect(groupFactory.createGroup(ownerAddress, tokenAddress, groupHash)).to.be.reverted;
+      });
+  
+  });   
+  
+  
 });
+  
+  
 
 
